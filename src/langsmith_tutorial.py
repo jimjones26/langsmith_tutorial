8c915fbe76a037a3f7b468a610116caae0c5367a
jjones <jimmy.jones@tutanota.com>
2023-11-29 19:50:37 -0700

    Add environment variables and API keys

--- src/langsmith_tutorial.py
+++ src/langsmith_tutorial.py
@@ -11,3 +11,15 @@ from langsmith import Client
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.smith import RunEvalConfig, run_on_dataset
+
+nest_asyncio.apply()
+
+# ------------------------------------------------------------------
+# Load api keys from .env file
+# ------------------------------------------------------------------
+
+load_dotenv(find_dotenv())
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "http://api.smith.langchain.com"
+os.environ["LANGCHAN_PROJECT"] = "langsmith-tutorial"
